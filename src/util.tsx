aad50f2c63faf0bbe645f999cf008f922ebf0501
Alex Johnson <alexmj212@gmail.com>
2022-01-13 22:41:44 -0500

    creat util file

---
+++ src/util.tsx
@@ -0,0 +1,42 @@
+import { LetterState } from "./App";
+
+const utilities = {
+  determineLetterClass: (letterState: LetterState) => {
+    if (letterState.noMatch && !letterState.positionMatch && !letterState.containMatch) {
+      return "no-match";
+    } else if (letterState.containMatch && !letterState.positionMatch) {
+      return "contain-match";
+    } else if (letterState.containMatch && letterState.positionMatch) {
+      return "position-match";
+    } else {
+      return "";
+    }
+  },
+  generateShareText: (guessArray: LetterState[][], puzzleNumber: number, fail = false): string => {
+    const rows = guessArray.filter((row) => !row.every((letter) => letter.letter === ""));
+    let shareText = `Word Guess `;
+    shareText += `${puzzleNumber} `;
+    shareText += `${fail ? "X" : rows.length}/6`;
+    shareText += "\n\n";
+
+    rows.forEach((row) => {
+      row.forEach((letter) => {
+        if (letter.noMatch && !letter.containMatch && !letter.positionMatch) {
+          shareText += "⬛";
+        } else if (letter.containMatch && !letter.positionMatch) {
+          shareText += "🟨";
+        } else if (letter.containMatch && letter.positionMatch) {
+          shareText += "🟩";
+        }
+      });
+      shareText += "\n";
+    });
+
+    return shareText;
+  },
+  previousGuess: (guess: string, guessArray: LetterState[][], mapPointer: number[]): boolean => {
+    return guessArray.slice(0, mapPointer[0]).some((row) => row.map((letter) => letter.letter).join("") === guess);
+  },
+};
+
+export default utilities;
