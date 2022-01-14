52189b2dc0898d070017602384d4111c29bca156
Alex Johnson <alexmj212@gmail.com>
2022-01-13 23:05:34 -0500

    improve cursor generator

--- src/GuessDisplay.tsx
+++ src/GuessDisplay.tsx
@@ -8,14 +8,18 @@ type GuessDisplayType = {
 };
 
 const GuessDisplay: React.FC<GuessDisplayType> = ({ guessMap, mapPointer }) => {
+  const cursor = (row: number, index: number, letter: LetterState): string => {
+    return row === mapPointer[0] && index === mapPointer[1] && !letter.letter ? "___" : letter.letter;
+  };
+
   return (
     <div>
       {guessMap.map((guessRow, row) => (
         <div key={row} className="flex flex-row justify-center">
           {guessRow.map((letter: LetterState, index) => (
             <div key={row + index}>
-              <div className={`button guess-box letter ${row < mapPointer[0] && !letter.containMatch && !letter.positionMatch ? "no-match" : ""} ${utilities.determineLetterClass(letter)}`}>
-                {row === mapPointer[0] && index === mapPointer[1] && !letter.letter ? "___" : letter.letter}
+              <div className={`button guess-box letter font-bold ${row < mapPointer[0] && !letter.containMatch && !letter.positionMatch ? "no-match" : ""} ${utilities.determineLetterClass(letter)}`}>
+                {cursor(row, index, letter)}
                 <br />
               </div>
             </div>
