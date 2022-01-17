c6cb1b819b9df4e0b79cc104ef5532dd5da5c1c9
Alex Johnson <alexmj212@gmail.com>
2022-01-16 23:58:13 -0500

    improve modal with confirmation dialog options

--- src/Modal.tsx
+++ src/Modal.tsx
@@ -1,16 +1,21 @@
-import React, { Fragment } from "react";
+import React, { Fragment, ReactFragment, useRef } from "react";
 import { Dialog, Transition } from "@headlessui/react";
+import { XIcon } from "@heroicons/react/outline";
 
 type ModalProps = {
   title: string;
   open: boolean;
   setOpen: (state: boolean) => void;
+  isStatic?: boolean;
+  confirmationDialog?: ReactFragment;
 };
 
-const Modal: React.FC<ModalProps> = ({ title, open, setOpen, children }) => {
+const Modal: React.FC<ModalProps> = ({ title, open, setOpen, isStatic = false, confirmationDialog, children }) => {
+  const closeRef = useRef(null);
+
   return (
     <Transition.Root show={open} as={Fragment}>
-      <Dialog as="div" className="fixed z-10 inset-0 overflow-y-auto" onClose={setOpen}>
+      <Dialog as="div" className="fixed z-10 inset-0 overflow-y-auto" onClose={setOpen} initialFocus={closeRef} static={isStatic}>
         <div className="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
           <Transition.Child as={Fragment} enter="ease-out duration-300" enterFrom="opacity-0" enterTo="opacity-100" leave="ease-in duration-200" leaveFrom="opacity-100" leaveTo="opacity-0">
             <Dialog.Overlay className="fixed inset-0 bg-neutral-800 bg-opacity-80 transition-opacity" />
@@ -30,20 +35,21 @@ const Modal: React.FC<ModalProps> = ({ title, open, setOpen, children }) => {
             leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
           >
             <div className="w-full sm:w-25 sm:max-w-lg inline-block align-bottom bg-white dark:bg-slate-700 rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:w-full">
-              <div className="px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
+              <div className="p-4 sm:p-6">
                 <div className="sm:flex sm:items-start">
                   <div className="w-full">
-                    <Dialog.Title as="h3" className="text-2xl leading-10 font-medium">
-                      {title}
-                    </Dialog.Title>
+                    <div className="flex justify-between">
+                      <Dialog.Title as="h3" className="text-2xl leading-10 font-medium">
+                        {title}
+                      </Dialog.Title>
+                      <button className="button-base" ref={closeRef}>
+                        <XIcon className="w-8 h-8" onClick={() => setOpen(false)}></XIcon>
+                      </button>
+                    </div>
                     <div className="mt-2">{children}</div>
+                    {confirmationDialog}
                   </div>
                 </div>
-              </div>
-              <div className="px-4 py-3 sm:px-6 flex justify-center">
-                <button type="button" className="button bg-slate-500 dark:bg-slate-500 text-white py-2" onClick={() => setOpen(false)}>
-                  OK
-                </button>
               </div>
             </div>
           </Transition.Child>
