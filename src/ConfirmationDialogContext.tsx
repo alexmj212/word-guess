ed038a23a3cab3d2a77de95bb62d58ae964f0bcf
Alex Johnson <alexmj212@gmail.com>
2022-02-05 17:15:31 -0500

    create generic confirmation context

---
+++ src/ConfirmationDialogContext.tsx
@@ -0,0 +1,59 @@
+import React, { useContext, useRef, useState } from "react";
+import Modal from "./Modal";
+
+type ConfirmationModalInterface = {
+  children?: React.ReactNode;
+  confirmText: string;
+  confirmButtonText: string;
+};
+
+interface ConfirmationModalContextInterface {
+  showConfirmation: () => Promise<boolean>;
+}
+
+export const ConfirmationModalContext = React.createContext<ConfirmationModalContextInterface | null>(null);
+
+const ConfirmationModalContextProvider = ({ children, confirmText, confirmButtonText }: ConfirmationModalInterface) => {
+  const [showConfirmationModal, setShowConfirmationModal] = useState(false);
+  const resolver = useRef<(v: boolean) => void>();
+
+  const handleShow = () => {
+    setShowConfirmationModal(true);
+
+    return new Promise<boolean>((resolve) => {
+      resolver.current = resolve;
+    });
+  };
+
+  const handleOk = () => {
+    setShowConfirmationModal(false);
+    resolver.current && resolver.current(true);
+  };
+
+  const handleCancel = () => {
+    setShowConfirmationModal(false);
+    resolver.current && resolver.current(false);
+  };
+
+  return (
+    <ConfirmationModalContext.Provider value={{ showConfirmation: handleShow }}>
+      {children}
+      <Modal open={showConfirmationModal} setOpen={setShowConfirmationModal} title="Confirm">
+        <div className="flex flex-col justify-center items-center">
+          <div className="my-4">{confirmText}</div>
+          <div className="flex flex-row flex-1 justify-center">
+            <button className="button py-2" onClick={handleCancel}>
+              Cancel
+            </button>
+            <button className="button-negative py-2" onClick={handleOk}>
+              {confirmButtonText}
+            </button>
+          </div>
+        </div>
+      </Modal>
+    </ConfirmationModalContext.Provider>
+  );
+};
+
+export const useConfirmationModalContext = () => useContext(ConfirmationModalContext);
+export default ConfirmationModalContextProvider;
