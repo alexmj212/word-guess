0df8dc212685e54b63e493af7c4c82ad7500079b
Alex Johnson <alexmj212@gmail.com>
2022-02-05 23:03:25 -0500

    improve cursor display

--- src/GuessDisplay.tsx
+++ src/GuessDisplay.tsx
@@ -7,9 +7,11 @@ type GuessDisplayType = {
   mapPointer: number[];
 };
 
+const cursorDisplay = "___";
+
 const GuessDisplay: React.FC<GuessDisplayType> = ({ guessMap, mapPointer }) => {
   const cursor = (row: number, index: number, letter: LetterState): string => {
-    return row === mapPointer[0] && index === mapPointer[1] && !letter.letter ? "___" : letter.letter;
+    return row === mapPointer[0] && index === mapPointer[1] && !letter.letter ? cursorDisplay : letter.letter;
   };
 
   return (
@@ -17,7 +19,10 @@ const GuessDisplay: React.FC<GuessDisplayType> = ({ guessMap, mapPointer }) => {
       {guessMap.map((guessRow, row) => (
         <div key={row} className="flex flex-row justify-center">
           {guessRow.map((letter: LetterState, index) => (
-            <div key={row + index} className={`button guess-box letter font-bold ${row < mapPointer[0] && !letter.containMatch && !letter.positionMatch ? "no-match" : ""} ${utilities.determineLetterClass(letter)}`}>
+            <div
+              key={row + index}
+              className={`${cursor(row, index, letter) === cursorDisplay ? "cursor" : "guess-box"} button  letter font-bold ${row < mapPointer[0] && !letter.containMatch && !letter.positionMatch ? "no-match" : ""} ${utilities.determineLetterClass(letter)} `}
+            >
               {cursor(row, index, letter)}
             </div>
           ))}
